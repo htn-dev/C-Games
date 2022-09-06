acdc06fb87411fe3e446b748cfaae30b94461f8f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-09-05 21:03:07 -0400

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -14,7 +14,6 @@ bool draw = false;
 //Function to show the current status of the gaming board
 
 void display_board(){
-
     //Rander Game Board LAYOUT
 
     cout<<"PLAYER - 1 [X]t PLAYER - 2 [O]nn";
